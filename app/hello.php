0017c643bca372bd41525785cac81c32898aede5
Alan Martins <alan-martins07@hotmail.com>
2024-08-16 23:57:36 -0300

    feat: testando conexao php

--- app/hello.php
+++ app/hello.php
@@ -1,8 +1,28 @@
+<?php
+// Inclua o arquivo MySQLConnection.php que está na pasta src
+require_once 'src/MySQLConnection.php';
+
+// Namespace da classe MySQLConnection
+use Application\DBConnection\MySQLConnection;
+?>
+
 <!doctype html>
 <html lang="pt-br">
-    <head>
-    </head>
-    <body>
-        <p><?php echo "Olá Mundo!!!" ?></p>
-    </body>
+<head>
+    <meta charset="UTF-8">
+    <title>Conexão com MySQL</title>
+</head>
+<body>
+    <p>
+        <?php 
+            try {
+                // Cria uma instância de MySQLConnection para conectar ao banco de dados
+                $db = new MySQLConnection();
+                echo "Conexão com o banco de dados foi bem-sucedida!";
+            } catch (\PDOException $e) {
+                echo "Falha na conexão: " . $e->getMessage();
+            }
+        ?>
+    </p>
+</body>
 </html>
